1dd51b91dff36ac5d5634c4edd3e8f52743014e8
Ray_Shangguan <a0963382999@gmail.com>
2022-09-21 16:14:23 +0800

    swap tools to new version

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from ModelTester import ModelTester
-from SimpleFileExplorer import SimpleFileExplorer
+from Ray_Python_Tools import SimpleFileExplorer
 from alive_progress import alive_bar
 tester = ModelTester("220916model.tflite","002.xlsx")
 path = input("輸入路徑\n")
 explorer = SimpleFileExplorer(path)
-files = explorer.getFiles()
+files = explorer.files
 with alive_bar(len(files)) as bar:
     for file in files:
         # print(path+'\\'+file)
